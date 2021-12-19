deb794f02b97851720ea6d7db9260102cb6f98e1
LOH567568 <90637155+LOH567568@users.noreply.github.com>
2021-12-19 13:35:33 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,127 @@
+// comment 
+/* comment 1,
+comment 2 */
+console.log("Hello, NEGR!")
+console.log("Hello,NEG'R")
+// String - строка 
+
+var number = 100
+let number2 = 25
+number3 = 10 
+number = number3
+console.log(number)
+
+const year = 2021 
+year2 = `Happy new ${year} year!`
+console.log(year2)
+new_name = prompt("как тебя зовут?")
+alert(`Здраствуйте, ${new_name} !`)
+
+scores = 0
+
+q1 = prompt("сколько месяцев в году?: a)12  b)10  c)5")
+if (q1== 'a' ||q1== 'а' ) {
+    scores = scores + 2 
+} else{
+    scores = scores - 2
+}
+
+q2 = prompt("кто спит с открытыми глазами?: a)рыбы  b)воробьи c)собаки")
+if (q2== 'a' ||q2== 'а' ) {
+    scores = scores + 2 
+}else{
+    scores = scores - 2
+}
+
+q3 = prompt("что из этого изобрили раньше?: a)часы  b)термометр c)телефон")
+if (q3== 'a' ||q3== 'а' ) {
+    scores = scores + 2 
+} else{
+    scores = scores - 2
+}
+
+q4 = prompt("что у машины под копотом?: a)100 лошадей b)пусто c)двигатель")
+if (q4== 'c' ||q4== 'с' ) {
+    scores = scores + 2 
+} else{
+    scores = scores - 2
+}
+
+q5 = prompt("кто самое быстрое животное? a)курица b)гепард c)пантера")
+if (q5== 'b' ||q5== 'б' ) {
+    scores = scores + 2 
+} else{
+    scores = scores - 2
+}
+
+q6 = prompt("смешать синий и жёлтый что получится?: a)белый b)зелёный c)красный")
+if (q6== 'b' ||q6== 'б' ) {
+    scores = scores + 2 
+} else{
+    scores = scores - 2
+}
+
+q7 = prompt("зимой и летом одним цветом что это? a)яблоко b)снеговик c)елка")
+if (q7== 'c' ||q7== 'с' ) {
+    scores = scores + 2 
+} else{
+    scores = scores - 2
+}
+
+q8 = prompt("LEGO это: a)конструктор b)машина c)колонка")
+if (q8== 'a' ||q8== 'а' ) {
+    scores = scores + 2 
+} else{
+    scores = scores - 2
+}
+
+q9 = prompt("сколько пальцев на руке? a)300 b)5 c)10")
+if (q9== 'b' ||q9== 'б' ) {
+    scores = scores + 2 
+} else{
+    scores = scores - 2
+}
+
+q10 = prompt("сколько в году летних месяцев? a)3 b)12 c)0")
+if (q10== 'a' ||q10== 'а' ) {
+    scores = scores + 2 
+} else{
+    scores = scores - 2
+}
+
+q11 = prompt("сколько цветов в радуге? a)10 b)7 c)5")
+if (q11== 'b' ||q11== 'б' ) {
+    scores = scores + 2 
+} else{
+    scores = scores - 2
+}
+
+q12 = prompt("какого числа Новый год? a)1 b)31 c)20")
+if (q12== 'b' ||q12== 'б' ) {
+    scores = scores + 2 
+} else{
+    scores = scores - 2
+}
+
+q13 = prompt("когда выпадает снег? a)летом b)весной c)зимой")
+if (q13== 'c' ||q13== 'с' ) {
+    scores = scores + 2 
+} else{
+    scores = scores - 2
+}
+
+q14 = prompt("сколько часов в сутках? a)24 b)14 c)5")
+if (q14== 'a' ||q14== 'а' ) {
+    scores = scores + 2 
+} else{
+    scores = scores - 2
+}
+
+q15 = prompt("сколько дней в неделе? a)10 b)7 c)5")
+if (q15== 'b' ||q15== 'б' ) {
+    scores = scores + 2 
+} else{
+    scores = scores - 2
+} 
+
+alert(`${scores} супер  `)
